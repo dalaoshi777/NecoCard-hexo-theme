b4d63e1ced2732d985960010998f62c330ebcc44
dalaoshi777 <dalaoshi777@gmail.com>
2022-11-01 12:47:48 +0800

    加入一些介绍

--- README.md
+++ README.md
@@ -1,9 +1,14 @@
 # NecoCard-hexo-theme
+
 一个简单的hexo主题
 
-主题浏览:[blog](https://dalaoshi777.github.io)
+[主题浏览](https://dalaoshi777.github.io)
 
 ## 如何使用？
 
-晚点写👍👍👍
-目前为第一版还不完善。咕咕咕
+1. clone此项目
+2. 把此项目丢进hexo的主题文件夹(在主题文件夹内直接clone更好)
+3. 备份一波此主题下的_config，根据里面给的注释做一下设置。
+4. 将hexo默认主题更换成此主题即可。
+
+主题并没有什么实用性，只是个简易的hexo静态博客主题，后续会继续更新完善一些功能。
--- source/js/main.js
+++ source/js/main.js
@@ -15,3 +15,24 @@ for (var i=0;i<links.length;i++)
 links[last].className = "hl";
 //----------------------------------------------------------------
 
+//-------------------------图片懒加载(需要浏览器支持)-------------------
+/*
+const images = document.querySelectorAll('img');
+
+const callback = entries => {
+   entries.forEach( entry => {
+      if( entry.isIntersecting ) {
+         const image = entry.target;
+         const data_src = image.getAttribute('data-src');
+         image.setAttribute('src',data_src);
+         observer.unobserve(image);
+         console.log('触发')
+      }
+   })
+}
+const observer = new IntersectionObserver( callback );
+images.forEach( image => {
+   observer.observe( image );
+});
+*/
+//----------------------------------------------------------------
